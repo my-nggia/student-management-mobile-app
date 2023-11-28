715024b7532314fc37d14721d0290b55cf74cc20
Ng T.Nguyen <nguyenthanhnguyen17102003@gmail.com>
2023-11-28 13:45:13 +0700

    Modifing activity_sign_in file add choose role and fix isValidAccount function add some attribute for role admin and manager feature

--- app/src/main/java/com/example/std_account_management/SignIn.java
+++ app/src/main/java/com/example/std_account_management/SignIn.java
@@ -9,6 +9,8 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,6 +24,7 @@ public class SignIn extends AppCompatActivity {
     EditText email_ed, password_ed;
     Button signInBtn;
     private FirebaseFirestore DB;
+    String role;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +36,22 @@ public class SignIn extends AppCompatActivity {
         password_ed = findViewById(R.id.password_sign_in);
         signInBtn = findViewById(R.id.sign_in_btn);
 
+        RadioGroup radioGroup = findViewById(R.id.btn_group_role);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId là ID của RadioButton được chọn trong RadioGroup
+                RadioButton radioButton = findViewById(checkedId);
+
+                if (radioButton != null) {
+                    // Lấy giá trị từ RadioButton được chọn
+                    String selectedValue = radioButton.getText().toString();
+                    // Sử dụng giá trị làm gì đó, ví dụ: hiển thị Toast
+                    role = selectedValue;
+                    Toast.makeText(SignIn.this, "Bạn đã chọn: " + selectedValue, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,7 +59,7 @@ public class SignIn extends AppCompatActivity {
                 String email = email_ed.getText().toString().trim();
                 String password = password_ed.getText().toString().trim();
                 // kiểm tra đã nhập đủ hết các giá trị
-                if (!isEmptyField(email, password)) {
+                if (!isEmptyField(email, password) && isAnyRadioButtonChecked(radioGroup)) {
                     // Kiểm tra tài khoản tồn tại
                     isValidAccount(email, password);
                 }
@@ -50,29 +69,61 @@ public class SignIn extends AppCompatActivity {
 
 
     private void isValidAccount(String email, String password) {
-        DB.collection("student").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot d : task.getResult()) {
-                        String db_email = d.getString("email");
-                        String db_pass = d.getString("pass");
-                        if (db_email.equalsIgnoreCase(email) && db_pass.equalsIgnoreCase(password)) {
-                            Intent mainActivityStudent = new Intent(getApplicationContext(), StudentActivity.class);
-                            mainActivityStudent.putExtra("user_email", db_email.toString());
-                            startActivity(mainActivityStudent);
-                            break;
-                        }
+        // Xác định collection dựa trên role
+        String collectionName = "";
+        Class<?> activityClass = null;
 
+        if (role.equalsIgnoreCase("admin")) {
+            collectionName = "admin";
+            activityClass = AdminActivity.class;
+        } else if (role.equalsIgnoreCase("manager")) {
+            collectionName = "manager";
+            activityClass = ManagerActivity.class;
+        } else {
+            collectionName = "student";
+            activityClass = StudentActivity.class;
+        }
+        final Class<?> finalActivityClass = activityClass;
+        final String finalCollectionName = collectionName;
+        // Tạo truy vấn để lấy dữ liệu từ collection
+        DB.collection(collectionName)
+                .whereEqualTo("email", email)
+                .whereEqualTo("pass", password)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (!task.getResult().isEmpty()) {
+                                // Tồn tại người dùng với thông tin đăng nhập đúng
+                                if(finalCollectionName.equalsIgnoreCase("student")) {
+                                    for (QueryDocumentSnapshot d : task.getResult()) {
+                                        String db_email = d.getString("email");
+                                        String db_pass = d.getString("pass");
+                                        if (db_email.equalsIgnoreCase(email) && db_pass.equalsIgnoreCase(password)) {
+                                            //Toast.makeText(SignIn.this, "OK " + email + " --- " + db_email, Toast.LENGTH_SHORT).show();
+                                            Intent mainActivityStudent = new Intent(getApplicationContext(), StudentActivity.class);
+                                            mainActivityStudent.putExtra("user_email", db_email.toString());
+                                            startActivity(mainActivityStudent);
+                                            break;
+                                        }
+                                    }
+                                }
+                                else {
+                                    Intent mainActivityIntent = new Intent(getApplicationContext(), finalActivityClass);
+                                    startActivity(mainActivityIntent);
+                                }
+                            }
+                            else {
+                                // Không tìm thấy người dùng
+                                Toast.makeText(SignIn.this, "Your account is invalid", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            // Xử lý lỗi khi truy cập cơ sở dữ liệu
+                            Toast.makeText(SignIn.this, "Some error when access database", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(SignIn.this, "Fail to get the data.", Toast.LENGTH_SHORT).show();
-            }
-        });
+                });
     }
 
     private boolean isEmptyField(String email, String password) {
@@ -84,5 +135,21 @@ public class SignIn extends AppCompatActivity {
             return true;
         } else
             return false;
+    }
+
+    private boolean isAnyRadioButtonChecked(RadioGroup radioGroup) {
+        int radioButtonCount = radioGroup.getChildCount();
+
+        for (int i = 0; i < radioButtonCount; i++) {
+            RadioButton radioButton = (RadioButton) radioGroup.getChildAt(i);
+
+            if (radioButton.isChecked()) {
+                // Có ít nhất một RadioButton được chọn
+                return true;
+            }
+        }
+        // Không có RadioButton nào được chọn
+        Toast.makeText(this, "Please choose your role", Toast.LENGTH_SHORT).show();
+        return false;
     }
 }
